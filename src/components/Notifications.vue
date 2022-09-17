a30dbe7727428886bc0383e03ed5e9d041d6f6db
Richard van Driest <richard.vandriest@outlook.com>
2022-09-17 20:53:14 +0200

    fix: show unread notifications count

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -7,17 +7,20 @@ import NewFollowerNotificationItem from './NotificationItems/NewFollowerNotifica
 import ReactionOnPictureNotificationItem from './NotificationItems/ReactionOnPictureNotificationItem.vue';
 import PrivateMessageNotificationItem from './NotificationItems/PrivateMessageNotificationItem.vue';
 import LeftYourGroupNotificationItem from './NotificationItems/LeftYourGroupNotificationItem.vue';
+import { computed } from 'vue';
 
 const store = useNotificationsStore();
 
 const { allNotifications } = storeToRefs(store);
 const { markAllAsRead } = store;
+
+const unreadNotificationsCount = computed(() => allNotifications.value.filter((notification) => !notification.read).length);
 </script>
 
 <template>
 	<div class="md:w-2/3 min-h-screen md:min-h-fit max-w-3xl w-full bg-white px-8 py-8 rounded-xl shadow-sm">
 		<div class="flex items-end justify-between">
-			<h1 class="font-extrabold text-2xl text-grayish-blue-600">Notifications <span class="bg-primary-blue text-white ml-2 px-3 rounded-md text-lg font-medium">{{allNotifications.length}}</span></h1>
+			<h1 class="font-extrabold text-2xl text-grayish-blue-600">Notifications <span class="bg-primary-blue text-white ml-2 px-3 rounded-md text-lg font-medium">{{unreadNotificationsCount}}</span></h1>
 			<button class="cursor-pointer text-gray-500 hover:text-primary-blue" @click="markAllAsRead">Mark all as read</button>
 		</div>
 		<div class="flex flex-col space-y-2 mt-6">
